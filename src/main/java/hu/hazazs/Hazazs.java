c78af84278c815227b4ad82ba394bc27d44555e6
Tibor Hazafi <hazafi.tibor@gmail.com>
2023-02-24 19:54:57 +0100

    Commit to start

--- src/main/java/hu/hazazs/Hazazs.java
+++ src/main/java/hu/hazazs/Hazazs.java
@@ -4,11 +4,7 @@ public class Hazazs {
 
     public static void main(String[] args) {
 
-        System.out.println("Commit 1");
 
-        System.out.println("Commit 2");
-
-        System.out.println("Commit 3");
 
     }
 
